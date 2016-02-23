29eb09c0df3b234d3a2c12b5de7b6772318a4ce1
Maksim Khortov <xoptov@mail.ru>
2016-02-23 14:50:56 +0300

    issue-40 добавлена страница для отправки сообщений в службу техподдержки

---
+++ src/AppBundle/Entity/Issue.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace AppBundle\Entity;
+
+class Issue
+{
+    /** @var integer */
+    private $id;
+
+    /** @var string */
+    private $subject;
+
+    /** @var string */
+    private $body;
+
+    /** @var string */
+    private $email;
+
+    /** @var \DateTime */
+    private $createdAt;
+
+    public function onCreate()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return Issue
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @param string $subject
+     * @return Issue
+     */
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param string $body
+     * @return Issue
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+}
